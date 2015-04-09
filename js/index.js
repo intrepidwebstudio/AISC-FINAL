4136795ec3af1c01dc219f8f55cc3a57115f65cd
intrepidwebstudio <sunil@inpws.com>
2015-04-09 10:57:33 +0530

    white line and other changes

--- js/index.js
+++ js/index.js
@@ -161,24 +161,32 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-          
+          StatusBar.backgroundColorByHexString("#1f1e2e");
 		
 		$('#abc div.ui-checkbox label').removeClass('ui-btn');
 		
 		registerDevice();
             if(navigator.network.connection.type == Connection.NONE){
               callbanner(); 
+			  loadlinksFromLocalDb(); 
+			  loadEventsFromLocalDb();
+			  loadContactsFromLocalDb(); 
+			  loadNewsletterFromLocalDb(); 
+			  loadNotificationsFromLocalDb();
             }else{
            		callbannerfromserver();
+				loadlinksfromserver(); 
+				loadEventsfromserver(); 
+				loadcontactsfromserver(); 
+				loadNewsletterfromserver();
+				loadNotificationsfromserver();
 				//console.log('connectin server');
+				
+				var mapid1='<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d3096.390826458215!2d80.24626718779895!3d12.981671817928133!2m2!1f0!2f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525d6450872cd3%3A0xa9a3700f3f4b67b0!2sAmerican+International+School!5e1!3m2!1sen!2sin!4v1428489014895"  frameborder="0" style="border:0; width:100%;"></iframe>';
+				$('#mapid').html(mapid1);    
+				
             }
 			browser_setting();
-			
-			loadlinks();
-			loadupcomingevents();
-			loadcontacts();
-			loadupcomingNewsletter();
-			loadnotifications();
 			loadpassword();
 			
 			
@@ -190,9 +198,6 @@ var app = {
 	
 	 }else{
 
-	var mapid1='<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d3096.390826458215!2d80.24626718779895!3d12.981671817928133!2m2!1f0!2f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525d6450872cd3%3A0xa9a3700f3f4b67b0!2sAmerican+International+School!5e1!3m2!1sen!2sin!4v1428489014895"  frameborder="0" style="border:0; width:100%;"></iframe>';
-	
-	$('#mapid').html(mapid1);    
 
 	
 	 }
