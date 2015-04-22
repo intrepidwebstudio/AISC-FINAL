99c095cf66017cc6d6a1d5eaf46bdb435c780e2e
intrepidwebstudio <sunil@inpws.com>
2015-04-22 19:06:58 +0530

    1234

--- js/index.js
+++ js/index.js
@@ -19,6 +19,7 @@
 
 var GOOGLE_PROJECT_ID = "";
 var PUSHAPPS_APP_TOKEN = "6f747f13-5c54-41f0-84c5-892d4c0ffdf6";
+var UserDeviceId='';
 
 /**
  * Register current device with PushApps
@@ -164,6 +165,7 @@ function removeTag() {
 function getDeviceId() {
     PushNotification.getDeviceId(function (deviceId) {
                                  alert("Your device id is: " + deviceId);
+								 UserDeviceId=deviceId;
 									savedeviceid(deviceId);
 							     }, function () {
                               //   alert("We could not get your device id. Please check your logs or contact our support team");
@@ -214,6 +216,8 @@ var app = {
 				loadlinksfromserver(); 
 				loadNewsletterfromserver();
 				//console.log('connectin server');
+				
+				
 				
 				var mapid1='<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d3096.390826458215!2d80.24626718779895!3d12.981671817928133!2m2!1f0!2f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525d6450872cd3%3A0xa9a3700f3f4b67b0!2sAmerican+International+School!5e1!3m2!1sen!2sin!4v1428489014895"  frameborder="0" style="border:0; width:100%;"></iframe>';
 				//$('#mapid').html(mapid1);    
